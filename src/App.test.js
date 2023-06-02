afa5556e2ce67d22ed72cb162524439a4cb2ce51
Anton Alokhin <anton.alyokhin1997@gmail.com>
2023-06-03 01:01:42 +0300

    03.06.2023.1:01

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render } from '@testing-library/prefer-screen-queries';
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
